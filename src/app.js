e9a17bfd95d1b80d6ccb2251ff34acbbcc6886ee
r4v3nz <vinicius.rjose@outlook.com>
2023-08-21 20:44:29 -0300

    criação da pagina para testes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const router = require("./router");
 const app = express();
 
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+
 app.use(router);
 
 module.exports = app;
--- src/router.js
+++ src/router.js
@@ -1,7 +1,22 @@
 const express = require("express");
-
+const isValidDomain = require('is-valid-domain');
 const router = express.Router();
 
-router.get("/search", (request, response) => response.status(200).send("Hello World"));
+router.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+router.get("/search/:domain", searchController);
+
+function searchController(req, res) {
+    domain = req.params.domain;
+    if (!isValidDomain(domain)) {
+        console.log("URL inválida:", domain);
+        res.status(400).send("URL inválida");
+        return;
+    }
+    console.log("URL VALIDA:", domain);
+    res.status(200).send("URL VALIDA");
+}
 
 module.exports = router;
